2f1324571e6f07827b919197fc682e6de4ecc415
wuyouliang <1241089954@qq.com>
2021-05-26 16:57:45 +0800

    增加设备信息获取

---
+++ app/src/main/java/com/example/study/demo/deviceInfo/DeviceInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.study.demo.deviceInfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeviceInvoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_invo);
+    }
+}
